d54bb8061793cc09c0240dd61a9b1933d81d30ea
Alessandra <alessavolpe@gmail.com>
2024-01-26 20:34:28 +0000

    add code to test KDL forward kinematics

--- opncvproj/src/ros_kin.cpp
+++ opncvproj/src/ros_kin.cpp
@@ -6,17 +6,41 @@
 #include <stdio.h>
 #include <iostream>
 
-using namespace KDL;
 
 int main() {
     //Definition of a kinematic chain & add segments to the chain
     KDL::Chain chain;
-    chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Vector(0.0,0.0,1.020))));
-    chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(0.0,0.0,0.480))));
-    chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(0.0,0.0,0.645))));
-    chain.addSegment(Segment(Joint(Joint::RotZ)));
-    chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(0.0,0.0,0.120))));
-    chain.addSegment(Segment(Joint(Joint::RotZ)));
+    // Will assume lenghts are in meters, and reference frame is at base of arm 
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ),KDL::Frame(KDL::Vector(0.0,0.0,0.0063))));
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotX),KDL::Frame(KDL::Vector(0.0,0.0,0.0102))));
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotX),KDL::Frame(KDL::Vector(0.0,0.0,0.0095))));
+    chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotX),KDL::Frame(KDL::Vector(0.0,0.0,0.0055))));
+
+    // Create solver based on kinematic chain
+    KDL::ChainFkSolverPos_recursive fksolver = KDL::ChainFkSolverPos_recursive(chain);
+
+    // Create joint array
+    unsigned int nj = chain.getNrOfJoints();
+    KDL::JntArray jointpositions = KDL::JntArray(nj);
+
+    // Assign some values to the joint positions, I'll assume position means angle in rads, with respect to previous link?
+    jointpositions(0) = 0.0;
+    jointpositions(1) = 0.785398;
+    jointpositions(2) = -0.785398;
+    jointpositions(3) = 1.570796;
+
+    // Create the frame that will contain the results
+    KDL::Frame cartpos;    
+ 
+    // Calculate forward position kinematics
+    bool kinematics_status;
+    kinematics_status = fksolver.JntToCart(jointpositions,cartpos);
+    if(kinematics_status>=0){
+        std::cout << cartpos <<std::endl;
+        printf("%s \n","Succes, thanks KDL!");
+    }else{
+        printf("%s \n","Error: could not calculate forward kinematics :(");
+    }
 
     return 0;
 }
