8d55869d41bcf0ae143713ed9350350ef2c8f87c
Alessandra <alessavolpe@gmail.com>
2024-01-30 15:33:04 +0000

    add inverse kinematics

--- opncvproj/src/ros_kin.cpp
+++ opncvproj/src/ros_kin.cpp
@@ -2,6 +2,8 @@
 #include <kdl/chainfksolver.hpp>
 #include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/frames_io.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainiksolverpos_nr.hpp>
 
 #include <stdio.h>
 #include <iostream>
@@ -23,24 +25,49 @@ int main() {
     unsigned int nj = chain.getNrOfJoints();
     KDL::JntArray jointpositions = KDL::JntArray(nj);
 
-    // Assign some values to the joint positions, I'll assume position means angle in rads, with respect to previous link?
+    // Assign some values to the joint positions, I'll assume position means angle in rads, with respect to previous link? it's servo angle, correct
+    // I will also assume the order of joints is the order in which I added the segments in chain? yes it is
     jointpositions(0) = 0.0;
     jointpositions(1) = 0.785398;
     jointpositions(2) = -0.785398;
     jointpositions(3) = 1.570796;
 
     // Create the frame that will contain the results
+    // output is going to be 4x3, the top 3x3 of the matrix is the rotation matrix, and the bottom 1x3 is the translation vector
     KDL::Frame cartpos;    
  
     // Calculate forward position kinematics
     bool kinematics_status;
     kinematics_status = fksolver.JntToCart(jointpositions,cartpos);
     if(kinematics_status>=0){
-        std::cout << cartpos <<std::endl;
+        std::cout << cartpos << std::endl;
         printf("%s \n","Succes, thanks KDL!");
     }else{
         printf("%s \n","Error: could not calculate forward kinematics :(");
     }
+
+    // Inverse: find the joint angle velocities
+    KDL::ChainIkSolverVel_pinv iksolverv(chain);//Inverse velocity solver
+    KDL::ChainIkSolverPos_NR iksolver(chain,fksolver,iksolverv,100,1e-6);//Maximum 100 iterations, stop at accuracy 1e-6
+
+    // Initial joints angles
+    KDL::JntArray q_init(chain.getNrOfJoints());
+    q_init(0) = 0.0;
+    q_init(1) = 0.0;
+    q_init(2) = 0.0;
+    q_init(3) = 0.0;
+
+    //Set destination frame
+    KDL::Frame F_dest = cartpos;
+
+    // Calculate joint angles using inverse kinematics
+    KDL::JntArray q_out(chain.getNrOfJoints());
+    int ret = iksolver.CartToJnt(q_init,F_dest,q_out);
+    std::cout << "IK Return: " << ret << std::endl;
+
+    for (int i = 0; i < nj; i++) 
+        std::cout << q_out(i) * 180 / KDL::PI << " degrees" << std::endl;
+    printf("%s \n","Succes, thanks KDL!");
 
     return 0;
 }
